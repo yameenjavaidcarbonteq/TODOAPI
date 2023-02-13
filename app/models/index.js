2d5cee7e3d6bb8da0b3aa66f5dc94013f1c8269e
Yameen Javaid <l164344@lhr.nu.edu.pk>
2023-02-14 00:03:21 +0500

    TODO API Preparations deleted some JUNK files

--- app/models/index.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('carbonteq_todo', 'root', '4675', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-module.exports = sequelize;
