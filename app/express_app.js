d009147f0118a29aed9863a77170d76ae6a96619
Yameen Javaid <l164344@lhr.nu.edu.pk>
2023-02-14 16:14:01 +0500

    preparations

--- app/express_app.js
+++ app/express_app.js
@@ -1,15 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
-const Route = require('./app/http/routes/route');
-
+const TodoRoutes = require('./http/routes/todo.routes');
+const AuthRoutes = require('./http/routes/auth.routes');
 
 const path = require('path');
 
-const authMiddleware = require("./app/http/middlewares/authMiddleware");
+const authMiddleware = require("./http/middlewares/authMiddleware");
 
 const app = express();
-const PORT = 8005;
+const PORT = 8006;
 
 app.use(express.json());
 app.use(express.text());
@@ -25,18 +25,11 @@ app.use(
   }));
 
 
-
-const todoRoutes = Route.create('todo');
-todoRoutes.createRoutes();
-const authRoutes = Route.create('user');
-authRoutes.createRoutes();
-
-
 // Use the router
-app.use('/', authRoutes.router);
+app.use('/', AuthRoutes);
 // Use the middleware to authenticate from session token in cookies
 app.use(authMiddleware);
-app.use('/', todoRoutes.router);
+app.use('/', TodoRoutes);
 
 
 
